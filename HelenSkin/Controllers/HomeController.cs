b95c810c9d518539f188d892f7fa493d69efc90a
NguyenTTung <cacppkk2211@gmail.com>
2024-03-22 15:37:37 +0700

    Chức năng đăng nhập

--- HelenSkin/Controllers/HomeController.cs
+++ HelenSkin/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+﻿using HelenSkin.Data.Access.Data;
 using HelenSkin.Model;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,11 +7,13 @@ namespace HelenSkin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -23,10 +26,62 @@ namespace HelenSkin.Controllers
             return View();
         }
 
+
+        public IActionResult DangNhap()
+        {
+            Response.Cookies.Delete("ID");
+            ViewData["HideHeader"] = true;
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult DangNhap(string tenTaiKhoan, string matKhau)
+        {
+            var nguoiDung = _db.db_NGUOI_DUNG.FirstOrDefault(x => x.TenTaiKhoan == tenTaiKhoan && x.MatKhau == matKhau && x.TrangThai == true);
+            var CheckTK = _db.db_NGUOI_DUNG.FirstOrDefault(x => x.TenTaiKhoan == tenTaiKhoan && x.TrangThai == true);
+            var CheckMK = _db.db_NGUOI_DUNG.FirstOrDefault(x => x.MatKhau == matKhau && x.TrangThai == true);
+            if(CheckTK == null)
+            {
+                ModelState.AddModelError("TenTaiKhoan", "Tên tài khoản không tồn tại");
+                return View();
+            }else if(CheckMK == null)
+            {
+                ModelState.AddModelError("MatKhau", "Mật khẩu không chính xác");
+                return View();
+            }else if (nguoiDung != null)
+            {
+
+                Response.Cookies.Append("ID", nguoiDung.MaND.ToString());
+
+                //TempData.Add("TenNguoiDung", nguoiDung.TenTaiKhoan);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+
+                TempData["ThongBaoDangNhap"] = "Tên tài khoản hoặc mật khẩu không đúng";
+                return View();
+
+            }
+        }
+
+
+
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+
+
     }
 }
